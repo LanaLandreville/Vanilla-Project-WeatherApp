e456c905faeceac6b33c396a5a1da36ee1bd2872
Lana Landreville <lana.landreville1@gmail.com>
2022-07-21 21:45:28 -0300

    forecast days

--- src/app.js
+++ src/app.js
@@ -21,36 +21,44 @@ function formatDate(timestamp) {
   return `${day},  ${hours}:${minutes}`;
 }
 
+function formatDay(timeStamp) {
+  let date = new Date(timeStamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
  <div class="col-2">
-  <div class="weather-forecast-date">
-${day}
+  <div class="weather-forecast-date">${formatDay(forecastDay.dt)}
   </div>
 
-<img src="http://openweathermap.org/img/wn/10d@2x.png" 
+<img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" 
 alt=""
 width="50"
 />
 <div class="weather-forecast-temperature">
 <span class="weather-forecast-temperature-max">
-  30°
+  ${Math.round(forecastDay.temp.max)}
 </span>
 <span class="weather-forecast-temperature-min">
-  | 20°
+  | ${Math.round(forecastDay.temp.min)}
 </span>
 </div>
 </div>
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
