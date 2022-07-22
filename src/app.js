09f15a94d467cf52fe8ccc260be180511b61a1a8
Lana Landreville <lana.landreville1@gmail.com>
2022-07-21 22:16:25 -0300

    remove console.log

--- src/app.js
+++ src/app.js
@@ -106,7 +106,6 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
 
 let form = document.querySelector("#search-form");
